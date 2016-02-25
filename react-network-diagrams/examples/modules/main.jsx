4a6a5cf13dff8bd54ba43ac6879d16e1a84bf9ff
root <root@unsecured.(none)>
2016-02-25 08:12:49 -0800

    Added react-network-diagrams

---
+++ react-network-diagrams/examples/modules/main.jsx
@@ -0,0 +1,48 @@
+/**
+ *  Copyright (c) 2015, The Regents of the University of California,
+ *  through Lawrence Berkeley National Laboratory (subject to receipt
+ *  of any required approvals from the U.S. Dept. of Energy).
+ *  All rights reserved.
+ *
+ *  This source code is licensed under the BSD-style license found in the
+ *  LICENSE file in the root directory of this source tree.
+ */
+
+/* eslint max-len:0 */
+
+import React from "react";
+import { render } from "react-dom";
+import { Router, Route, IndexRoute } from "react-router";
+
+import App from "./app.jsx";
+import Intro from "./intro.jsx";
+import MapLegendExample from "./legend_example";
+import TrafficMapExample from "./trafficmap_example";
+import BESDataMap from "./besdatamap";
+import BasicCircuitExample from "./basic_circuit_example";
+import ConcatenatedCircuitExample from "./concatenated_circuit_example";
+import ParallelCircuitExample from "./parallel_circuit_example";
+import PatchPanelExample from "./patch_panel_example";
+import EndpointExample from "./endpoint_example";
+import ConnectionExample from "./connection_example";
+import EditorExample from "./editor_example";
+import PathsExample from "./paths_example";
+
+render((
+    <Router>
+        <Route path="/" component={TrafficMapExample}>
+            <IndexRoute component={Intro}/>
+            <Route path="legend" component={MapLegendExample} />
+            <Route path="trafficmap" component={TrafficMapExample} />
+            <Route path="besdatamap" component={BESDataMap} />
+            <Route path="basicCircuit" component={BasicCircuitExample} />
+            <Route path="concatenatedCircuit" component={ConcatenatedCircuitExample} />
+            <Route path="parallelCircuit" component={ParallelCircuitExample} />
+            <Route path="patchPanel" component={PatchPanelExample} />
+            <Route path="endpoint" component={EndpointExample} />
+            <Route path="connection" component={ConnectionExample} />
+            <Route path="editor" component={EditorExample} />
+            <Route path="paths" component={PathsExample} />
+        </Route>
+    </Router>
+), document.getElementById("content"));
